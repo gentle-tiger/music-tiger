b75d84152b7798e9784b332edfe5afb417f91d3c
Choi Hojin <jkhh9725@gmail.com>
2025-04-19 19:57:19 +0900

    🎉 Initial commit: MusicTiger 프로젝트 생성

---
+++ server/src/main/java/com/musictiger/MusictigerApplication.java
@@ -0,0 +1,13 @@
+package com.musictiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusictigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusictigerApplication.class, args);
+	}
+
+}
